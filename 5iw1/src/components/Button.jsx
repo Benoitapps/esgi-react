e85c14eba8678b1b6323db6456b2426d1faf2d1d
Karl MARQUES <kmarques@vetixy.com>
2023-09-14 14:42:45 +0200

    initial commit

---
+++ 5iw1/src/components/Button.jsx
@@ -0,0 +1,37 @@
+export default function Button({
+  title,
+  variant,
+  component: Component = "button",
+  onClick,
+  children
+}) {
+  const style = {
+    borderRadius: 0,
+    color: "black",
+    backgroundColor: "white",
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: "1px",
+  };
+  switch (variant) {
+    case "rounded":
+      style.borderRadius = "5px";
+      break;
+    case "circle":
+      style.borderRadius = "50%";
+      style.width = "50px";
+      style.height = "50px";
+      style.display = "flex";
+      style.justifyContent = "center";
+      style.alignItems = "center";
+      title = title[0];
+      break;
+    case "text":
+      style.border = "none";
+      style.backgroundColor = "transparent";
+      style.color = "pink";
+      Component = "a";
+  }
+
+  return <Component style={style} onClick={onClick}>{children || title}</Component>;
+}
